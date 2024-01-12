c4222c2f9f9c682dfc723cfbfceb3fb190efc92c
Diego Rodríguez <rodriguezpeligerod@gmail.com>
2024-01-12 13:57:43 +0100

    Index vehiculo creado

---
+++ MVC2024/MVC2024/Controllers/VehiculoController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MVC2024.Models;
+
+namespace MVC2024.Controllers
+{
+    public class VehiculoController : Controller
+    { 
+        public Contexto contexto { get; }
+
+        public VehiculoController(Contexto contexto)
+        {
+            this.contexto = contexto;
+        }
+        // GET: VehiculoController
+        public ActionResult Index()
+        {
+
+            return View(contexto.VehiculoModelo.Include(x => x.Serie).ToList());
+        }
+
+        // GET: VehiculoController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: VehiculoController/Create
+        public ActionResult Create()
+        {
+            ViewBag.serieID = new SelectList(contexto.SerieModelo, "ID", "nomSerie");
+            return View();
+        }
+
+        // POST: VehiculoController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(VehiculoModelo vehiculo)
+        {
+            contexto.VehiculoModelo.Add(vehiculo);
+            contexto.Database.EnsureCreated();
+            contexto.SaveChanges();
+
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: VehiculoController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: VehiculoController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: VehiculoController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: VehiculoController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
